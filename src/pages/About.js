ba298e01e533b5dbd9958bfbd16755a4644bc776
Abdallah Abas <abdallahabasabish@gmail.com>
2026-07-16 16:17:50 +0300

    Delete src directory

--- src/pages/About.js
+++
@@ -1,5 +0,0 @@
-export default class About {
-  render() {
-    return `<div class="container mx-auto px-4 py-20"><h1 class="text-4xl font-bold">عن المكتب</h1><p class="mt-4 text-gray-600">هنا محتوى صفحة عن المكتب...</p></div>`
-  }
-}
