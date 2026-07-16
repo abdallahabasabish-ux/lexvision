51d5a5f8912cb52f714dbb37a03a5a3d90d680e6
Abdallah Abas <abdallahabasabish@gmail.com>
2026-07-16 15:41:13 +0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,26 @@
-/** @type {import('tailwindcss').Config} */
+ت /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{js,html}'],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,html}"
+  ],
   theme: {
     extend: {
       colors: {
-        primary: '#393536',
-        secondary: '#bd9d60',
-        'secondary-light': '#e1b076',
-        white: '#FFFFFF',
-        success: '#16A34A',
-        danger: '#DC2626',
-        warning: '#F59E0B'
+        primary: "#393536",
+        secondary: "#bd9d60",
+        "secondary-light": "#e1b076",
+        white: "#FFFFFF",
+        success: "#16A34A",
+        danger: "#DC2626",
+        warning: "#F59E0B"
       },
       fontFamily: {
-        arabic: ['Cairo', 'sans-serif'],
-        english: ['Inter', 'sans-serif']
+        arabic: ["Cairo", "sans-serif"],
+        english: ["Inter", "sans-serif"]
       },
       borderRadius: {
-        xl: '1.25rem'
+        xl: "1.25rem"
       }
     }
   },
