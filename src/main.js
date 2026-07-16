51d2c5bf75b95f019c27c9ed5cfebf666047a371
Abdallah Abas <abdallahabasabish@gmail.com>
2026-07-16 14:07:14 +0300

    Create main.js

---
+++ src/main.js
@@ -0,0 +1,10 @@
+import './assets/css/main.css'
+import App from './App.js'
+import { initI18n } from './i18n.js'
+
+// تهيئة اللغة
+initI18n('ar')
+
+// تشغيل التطبيق
+const app = new App()
+app.mount('#app')
